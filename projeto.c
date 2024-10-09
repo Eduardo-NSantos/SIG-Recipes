aec4a10da7d3042e65b10ac07a7c052cb19f5dd1
MoisesLuc <moisesbrats98@gmail.com>
2024-10-09 17:08:37 -0300

    Cria Função de Navegação entre Menu Principal e dos Cozinheiros

--- projeto.c
+++ projeto.c
@@ -19,12 +19,13 @@
 
 // --== * Assinaturas * ==-- //
 void menu_inicio(void);
-void menu_principal(void);
+char menu_principal(void);
 void menu_informacoes(void);
 void menu_equipe(void);
 void autenticacao(void);
 void cadastro(void);
-void menu_cozinheiro(void);
+void modulo_cozinheiro(void);
+char menu_cozinheiro(void);
 void cadastra_receita(void);
 void altera_receita(void);
 void deleta_receita(void);
@@ -51,12 +52,22 @@ void menu_receitas(void);
 
 // --== * Função Principal * ==-- //
 int main(void) {
+    char opcao;
+    
     menu_inicio();
     cadastro();
     autenticacao();
-    menu_principal();
 
-    menu_cozinheiro();
+    do {
+        opcao = menu_principal();
+        switch (opcao) {
+            case '1':
+                modulo_cozinheiro();
+                break;
+        }    
+    } while (opcao != '0');
+    
+
     cadastra_receita();
     altera_receita();
     deleta_receita();
@@ -78,6 +89,16 @@ int main(void) {
 
 
 
+void modulo_cozinheiro(void) {
+    char opcao;
+
+    do {
+        opcao = menu_cozinheiro();
+    } while (opcao != '0');
+}
+
+
+
 // --== * Funções de Cor * ==-- //
 // Retiradas de: https://www.theurbanpenguin.com/4184-2/
 void yellow() {
@@ -91,7 +112,6 @@ void green() {
 void reset_color() { 
     printf("\033[0m");
 }
-
 
 
 
@@ -122,7 +142,8 @@ void menu_inicio(void) {
 
 
 // --== * Menu Principal * ==-- //
-void menu_principal(void) {
+char menu_principal(void) {
+    char opcao;
     system("clear||cls");
     yellow();
     printf("\n");
@@ -141,17 +162,18 @@ void menu_principal(void) {
     printf("//             (* 0 *) ---===       * Sair *        ===---                    //\n");
     printf("//                                                                            //\n");
     printf("//((((((((((((((((((((((((((((((((((((****))))))))))))))))))))))))))))))))))))//\n");
-    printf("\n");
-    printf("                       -------======= *  * =======--------                      \n");
-    printf("                     ---== Aperte ENTER para continuar ==---                    \n");
-    getchar();
+    printf("                       -------======= *  * =======-------                       \n");
+    printf("               --== Escolha o destino desejado: ");
+    scanf(" %c", &opcao);
+    return opcao;
     reset_color();
 }
 
 
 
 // --== * Menu do Cozinheiro * ==-- //
-void menu_cozinheiro(void) {
+char menu_cozinheiro(void) {
+    char opcao;
     system("clear||cls");
     yellow();
     printf("\n");
@@ -170,10 +192,10 @@ void menu_cozinheiro(void) {
     printf("//           (* 0 *) ---===      * Retornar *      ===---                     //\n");
     printf("//                                                                            //\n");
     printf("//((((((((((((((((((((((((((((((((((((****))))))))))))))))))))))))))))))))))))//\n");
-    printf("\n");
     printf("                       -------======= *  * =======-------                       \n");
-    printf("                     ---== Aperte ENTER para continuar ==---                    \n");
-    getchar();
+    printf("               --== Escolha o destino desejado: ");
+    scanf(" %c", &opcao);
+    return opcao;
     reset_color();
 }
 
