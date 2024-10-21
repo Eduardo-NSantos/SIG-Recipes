a7f29044fd7f9227a9d31e4544f0a4fa4b5b67c9
Isaqueguicar <isaqueguimaraes388@gmail.com>
2024-10-21 13:47:40 -0300

    Separa o Módulo de Receitas do Módulo Principal

---
+++ relatorio.h
@@ -0,0 +1,17 @@
+//((((((((((((((((((((((((((((((((((((****))))))))))))))))))))))))))))))))))))//
+//                                                                            //
+//                Universidade Federal do Rio Grande do Norte                 //
+//                         Disciplina de Programação                          //
+//                          Prof, Flavius Gorgônio                            //
+//                      Moisés Lucena, Isaque Guimarães                       //
+//                                                                            //
+//((((((((((((((((((((((((((((((((((((****))))))))))))))))))))))))))))))))))))//
+//                                                                            //
+//                                Versão 0.4                                  //
+//                                                                            //
+//((((((((((((((((((((((((((((((((((((****))))))))))))))))))))))))))))))))))))//
+
+
+// --== * Assinaturas * ==-- //
+void modulo_relatorio(void);
+char menu_relatorio(void);
